1cf78d9105ba37532c4654b9da8f7a28070401c4
eitanschreiber123 <eitanschreiber97@gmail.com>
2025-01-20 18:59:48 -0500

    first

--- src/app/page.js
+++ src/app/page.js
@@ -5,90 +5,20 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1 style={{textAlign:'center'}}>SMJB Capital Investments</h1>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/dryden.png"
+          alt="Dryden image"
+          style={{width:'100vw'}}
+          height={200}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <article style={{margin:'50px 0',padding:'0 20px'}}>SMJB Capital Investments is a real estate acquisitions firm focused on buying, renovating ans then re-selling single family homes in the states of Georgia and Alabama.</article>
+        <article style={{padding:'0 20px'}}>Please contact us for further information on our services at info@smjbcapitalinvestments.com</article>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer style={{backgroundColor:'black',flex:1,color:'white',display:'flex',width:'100vw',justifyContent:'space-evenly',alignItems:'center',padding:'20px 0',flexWrap:'wrap'}}>
+       <p>Contact us: </p><p>info@smjbcapitalinvestments.com</p>
       </footer>
     </div>
   );
