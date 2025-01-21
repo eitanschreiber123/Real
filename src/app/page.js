8aec1ec463b637ff50dc33e422a5ddd57c781c13
eitanschreiber123 <eitanschreiber97@gmail.com>
2025-01-20 19:19:17 -0500

    link

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -15,10 +16,10 @@ export default function Home() {
           priority
         />
         <article style={{margin:'50px 0',padding:'0 20px'}}>SMJB Capital Investments is a real estate acquisitions firm focused on buying, renovating ans then re-selling single family homes in the states of Georgia and Alabama.</article>
-        <article style={{padding:'0 20px'}}>Please contact us for further information on our services at info@smjbcapitalinvestments.com</article>
+        <article style={{padding:'0 20px'}}>Please contact us for further information on our services at <Link style={{color:'#0000EE',textDecoration:'underline'}} href="mailto:info@smjbcapitalinvestments.com">info@smjbcapitalinvestments.com</Link></article>
       </main>
       <footer style={{backgroundColor:'black',flex:1,color:'white',display:'flex',width:'100vw',justifyContent:'space-evenly',alignItems:'center',padding:'20px 0',flexWrap:'wrap'}}>
-       <p>Contact us: </p><p>info@smjbcapitalinvestments.com</p>
+       <p>Contact us: </p><Link href="mailto:info@smjbcapitalinvestments.com">info@smjbcapitalinvestments.com</Link>
       </footer>
     </div>
   );
